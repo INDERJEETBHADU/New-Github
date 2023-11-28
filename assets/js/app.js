fdab39a1ad88e7ec755820ee8894874d1fbb1f40
INDERJEETBHADU <bishnoiinderjeet580@gmail.com>
2023-11-28 14:15:33 +0530

    page_completed

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,38 @@
+const Preload = document.getElementById("preload")
+setTimeout(() => {
+  document.body.classList.remove("overflow-hidden");
+  Preload.classList.add("d-none")
+}, "5000");
 function openNav() {
-    document.getElementById("navbar").classList.toggle("start-0");
-    document.body.classList.toggle("overflow_hidden");
-    document.querySelector(".menu").classList.toggle("cross")
+  document.getElementById("navbar").classList.toggle("start-0");
+  document.body.classList.toggle("overflow_hidden");
+  document.querySelector(".menu").classList.toggle("cross")
+}
+
+// vishal___js
+$('.one-time').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1.7,
+  autoplay: true,
+  responsive: [{
+    breakpoint: 768,
+    settings: {
+      arrows: false,
+      centerMode: true,
+      centerPadding: '40px',
+      slidesToShow: 1
+    }
+  },
+  {
+    breakpoint: 480,
+    settings: {
+      arrows: false,
+      centerMode: true,
+      centerPadding: '40px',
+      slidesToShow: 1
+    }
   }
-  
+  ]
+});
